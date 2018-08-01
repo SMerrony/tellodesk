7c7ee4440f202cd7f54a6db4e483fc9d73d46751
Stephen Merrony <steve@stephenmerrony.co.uk>
2018-08-01 08:52:58 +0200

    Populate menu bar

--- tdApp.go
+++ tdApp.go
@@ -8,17 +8,24 @@ import (
 type tdApp struct {
 	*application.Application
 	// gui                tdGUI
-	menuBar            *gui.Menu
-	fileMenu, helpMenu *gui.Menu
-	panel              *gui.Panel
-	label              *gui.Label
+	menuBar                                                          *gui.Menu
+	fileMenu, droneMenu, flightMenu, videoMenu, imagesMenu, helpMenu *gui.Menu
+	panel                                                            *gui.Panel
+	label                                                            *gui.Label
 }
 
 func (app *tdApp) setup() {
 	app.Gui().SetLayout(gui.NewVBoxLayout())
+
+	app.buildMenu()
+	app.Gui().Add(app.menuBar)
+
+}
+
+func (app *tdApp) buildMenu() {
 	app.menuBar = gui.NewMenuBar()
 	app.menuBar.SetLayoutParams(&gui.VBoxLayoutParams{Expand: 0, AlignH: gui.AlignWidth})
-	app.Gui().Add(app.menuBar)
+
 	app.fileMenu = gui.NewMenu()
 	app.fileMenu.AddOption("Exit").SetId("exit").Subscribe(gui.OnClick, func(string, interface{}) {
 		app.Quit()
@@ -27,10 +34,36 @@ func (app *tdApp) setup() {
 
 	//app.menuBar.AddSeparator()
 
+	app.droneMenu = gui.NewMenu()
+	app.droneMenu.AddOption("Connect")
+	app.droneMenu.AddOption("Disconnect").SetEnabled(false)
+	app.menuBar.AddMenu("Drone", app.droneMenu)
+
+	app.flightMenu = gui.NewMenu()
+	app.flightMenu.AddOption("Take-off")
+	app.flightMenu.AddOption("Throw Take-off")
+	app.flightMenu.AddOption("Land")
+	app.flightMenu.AddOption("Palm Land")
+	app.flightMenu.AddSeparator()
+	app.flightMenu.AddOption("Sports (Fast) Mode")
+	app.menuBar.AddMenu("Flight", app.flightMenu)
+
+	app.videoMenu = gui.NewMenu()
+	app.videoMenu.AddOption("Start Video View")
+	app.videoMenu.AddOption("Stop Video View")
+	app.videoMenu.AddSeparator()
+	app.videoMenu.AddOption("Record Video")
+	app.menuBar.AddMenu("Video", app.videoMenu)
+
+	app.imagesMenu = gui.NewMenu()
+	app.imagesMenu.AddOption("Take Photo")
+	app.imagesMenu.AddOption("Save Photo(s)").SetEnabled(false)
+	app.menuBar.AddMenu("Images", app.imagesMenu)
+
 	app.helpMenu = gui.NewMenu()
+	app.helpMenu.AddOption("Online Help")
 	app.helpMenu.AddSeparator()
 	app.helpMenu.AddOption("About")
 	app.menuBar.AddMenu("Help", app.helpMenu)
-	//app.Gui().Add(app.gui.window)
 
 }
