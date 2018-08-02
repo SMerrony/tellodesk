798e85480260e491060945b8de8e7df32078dd64
Stephen Merrony <steve@stephenmerrony.co.uk>
2018-08-02 12:03:02 +0200

    Improve dialogWindow modality etc.

--- dialogWindow.go
+++ dialogWindow.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/g3n/engine/gui"
+	"github.com/g3n/engine/gui/assets/icon"
 )
 
 type severityType int
@@ -14,24 +15,30 @@ const (
 )
 
 const (
-	dlgWidth, dlgHeight = 300, 300
+	dlgWidth, dlgHeight = 300, 200
 )
 
-func alertDialog(sev severityType, msg string) *gui.Window {
+type dialogWin struct{}
+
+func (t *dialogWin) alertDialog(app *tdApp, sev severityType, msg string) *gui.Window {
 	win := gui.NewWindow(dlgWidth, dlgHeight)
 	win.SetResizable(false)
 	win.SetPaddings(4, 4, 4, 4)
 
-	var titleStr string
+	var titleStr, iconStr string
 	switch sev {
 	case infoSev:
 		titleStr = "Information"
+		iconStr = string(icon.Info)
 	case warningSev:
 		titleStr = "Warning"
+		iconStr = string(icon.Warning)
 	case errorSev:
 		titleStr = "Error"
+		iconStr = string(icon.Error)
 	case criticalSev:
 		titleStr = "Critical Error"
+		iconStr = string(icon.Error)
 	}
 	win.SetTitle(titleStr)
 
@@ -40,13 +47,22 @@ func alertDialog(sev severityType, msg string) *gui.Window {
 	win.SetLayout(lay)
 
 	msgLab := gui.NewImageLabel(msg)
+	msgLab.SetIcon(iconStr)
 	msgLab.SetLayoutParams(&gui.VBoxLayoutParams{Expand: 2, AlignH: gui.AlignWidth})
 	win.Add(msgLab)
 
 	ok := gui.NewButton("OK")
 	ok.SetLayoutParams(&gui.VBoxLayoutParams{Expand: 0, AlignH: gui.AlignCenter})
-	ok.Subscribe(gui.OnClick, func(e string, ev interface{}) { win.SetVisible(false) })
+	ok.Subscribe(gui.OnClick, func(e string, ev interface{}) {
+		app.Gui().SetModal(nil)
+		app.Gui().Remove(win)
+	})
 	win.Add(ok)
+
+	win.SetCloseButton(false)
+
+	app.Gui().Add(win)
+	app.Gui().SetModal(win)
 
 	return win
 }
--- droneCBs.go
+++ droneCBs.go
@@ -3,12 +3,21 @@ package main
 func (app *tdApp) connectCB(s string, i interface{}) {
 	err := drone.ControlConnectDefault()
 	if err != nil {
-		app.Gui().Add(alertDialog(
+		// 		app.Gui().Add(alertDialog(
+		// 			errorSev,
+		// 			`Could not connect to Drone.
+
+		// Check that you have a Wifi connection
+		// to the Tello network.`))
+		var ad dialogWin
+		ad.alertDialog(
+			app,
 			errorSev,
 			`Could not connect to Drone.
 
 Check that you have a Wifi connection 
-to the Tello network.`))
+to the Tello network.`)
+
 		return // Comment this for GUI testing
 	}
 	app.disconnectItem.SetEnabled(true)
